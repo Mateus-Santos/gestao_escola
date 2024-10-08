4ee824eec144d4b5213180444f97972686f04c75
Mateus-Santos <mateusantosms11@gmail.com>
2024-10-08 16:54:49 -0300

    Melhorias na maneira de filtrar alunos para atualizar apostila

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import streamlit as st
 import os
-from apis.api_modulos import update_modulos
-from apis.api_modulos import local_Sheets_Modulos
 
 # Mensagem Principal.
 st.title("Bem-vindo a tela de relatórios do Instituto Mix.")
@@ -17,29 +15,3 @@ def update_database(caminho_arquivo, uploaded_file):
         with open(caminho_arquivo, "wb") as file:
             file.write(uploaded_file.getbuffer())
         st.success(f"O arquivo foi substituído com sucesso em {caminho_arquivo}")
-
-def news_alunos(novos_alunos):
-    if novos_alunos is not None:
-        novos_alunos_df = pd.read_excel(novos_alunos)
-        gsheets = local_Sheets_Modulos()
-        #Corrigindo colunas do gsheets;
-        colunas = gsheets[0]
-        material = pd.DataFrame(data=gsheets, columns=colunas)
-        material = material[1:].reset_index(drop=True)
-        #Filtro invertido
-        filtro = ~novos_alunos_df['Aluno'].isin(material['ESTUDANTES'])
-        valores_nao_encontrados = novos_alunos_df['Aluno'][filtro]
-        # Criar um novo DataFrame com valores não encontrados para adicionar ao final de df1
-        new_material = pd.DataFrame({'ESTUDANTES': valores_nao_encontrados})
-        # Adicionar os valores não encontrados ao final de 'coluna1' de df1
-        material = pd.concat([material, new_material], ignore_index=True)
-        material = material.fillna('A')
-        st.write('Novos nomes:')
-        st.write(valores_nao_encontrados)
-        envio = material.values.tolist()  # Cria uma lista de listas a partir do DataFrame
-        update_modulos(envio)
-        return st.write(material)
-
-st.title("Atualize base de dados com novos alunos para a análise de módulos:")
-novos_alunos = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'], key="novos_alunos")
-news_alunos(novos_alunos)
--- pages/📚3_entrega_apostila_interativo.py
+++ pages/📚3_entrega_apostila_interativo.py
@@ -3,6 +3,7 @@ import streamlit as st
 import io
 
 from apis.api_modulos import local_Sheets_Modulos
+from apis.api_modulos import update_modulos
 
 def entrega_Modulos():
     gsheets = local_Sheets_Modulos()
@@ -34,4 +35,30 @@ def entrega_Modulos():
     )
     st.dataframe(qtd_nao_entregues)
 
+def news_alunos(novos_alunos):
+    if novos_alunos is not None:
+        df = pd.read_excel(novos_alunos)
+        novos_alunos_df = df[(df['Status'] == 'Ativo') & (df['Tipo Contrato'] == 'Interativo')]
+        gsheets = local_Sheets_Modulos()
+        #Corrigindo colunas do gsheets;
+        colunas = gsheets[0]
+        material = pd.DataFrame(data=gsheets, columns=colunas)
+        material = material[1:].reset_index(drop=True)
+        #Filtro invertido
+        filtro = ~novos_alunos_df['Aluno'].isin(material['ESTUDANTES'])
+        valores_nao_encontrados = novos_alunos_df['Aluno'][filtro]
+        # Criar um novo DataFrame com valores não encontrados para adicionar ao final de df1
+        new_material = pd.DataFrame({'ESTUDANTES': valores_nao_encontrados})
+        # Adicionar os valores não encontrados ao final de 'coluna1' de df1
+        material = pd.concat([material, new_material], ignore_index=True)
+        material = material.fillna('A')
+        st.write('Novos nomes:')
+        st.write(valores_nao_encontrados)
+        envio = material.values.tolist()  # Cria uma lista de listas a partir do DataFrame
+        update_modulos(envio)
+        return st.write(material)
+
+st.title("Atualize base de dados com novos alunos para a análise de módulos:")
+novos_alunos = st.file_uploader("Escolha um arquivo Excel", type=['xlsx', 'xls'], key="novos_alunos")
 entrega_Modulos()
+news_alunos(novos_alunos)
